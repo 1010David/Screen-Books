73a50f3511ffd8f4575b4e1e49cc7e3892c6c8ed
David <j.david.beltran.m@gmail.com>
2024-11-11 10:14:30 -0500

    Arreglando problema de CRLF a LF

--- src/main/java/com/aluraoracle/screenbook/ScreenbookApplication.java
+++ src/main/java/com/aluraoracle/screenbook/ScreenbookApplication.java
@@ -1,13 +1,30 @@
+// En un archivo separado: LiteraturaApplication.java
 package com.aluraoracle.screenbook;
 
+import com.aluraoracle.screenbook.principal.Principal;
+import com.aluraoracle.screenbook.repository.AutoresRepository;
+import com.aluraoracle.screenbook.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenbookApplication {
+class LiteraturaApplication implements CommandLineRunner {
+
+	@Autowired
+	private AutoresRepository autoresRespositorio;
+
+	@Autowired
+	private LibroRepository libroRepositorio;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScreenbookApplication.class, args);
+		SpringApplication.run(LiteraturaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(autoresRespositorio, libroRepositorio);
+		principal.muestraMenu();
+	}
 }
